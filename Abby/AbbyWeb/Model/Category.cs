fa2cfd0d5a3890d54d34aaeed304dfb56adbe584
Petar Kobakov <pkobakov@gmail.com>
2022-11-08 18:52:34 +0200

    Display Name and Range Annotation

--- Abby/AbbyWeb/Model/Category.cs
+++ Abby/AbbyWeb/Model/Category.cs
@@ -8,6 +8,9 @@ namespace AbbyWeb.Model
         public int Id { get; set; }
         [Required]
         public string Name  { get; set; }
+
+        [Display(Name ="Display Order")]
+        [Range(1,100, ErrorMessage ="The Display Order Value should be between 1 and 100")]
         public int DisplayOrder { get; set; }
     }
 }
