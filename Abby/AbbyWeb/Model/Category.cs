7b91449746ef75530ac6efe5ddfb3aae8d25ad90
Petar Kobakov <pkobakov@gmail.com>
2022-11-08 17:47:20 +0200

    Using BootsWatch

--- Abby/AbbyWeb/Model/Category.cs
+++ Abby/AbbyWeb/Model/Category.cs
@@ -8,7 +8,6 @@ namespace AbbyWeb.Model
         public int Id { get; set; }
         [Required]
         public string Name  { get; set; }
-        []
         public int DisplayOrder { get; set; }
     }
 }
