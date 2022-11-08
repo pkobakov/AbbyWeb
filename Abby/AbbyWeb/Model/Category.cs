429212a6eda3576aabefa0309e1b94033def8618
Petar Kobakov <pkobakov@gmail.com>
2022-11-08 17:11:38 +0200

    Create Property and Bind Property

--- Abby/AbbyWeb/Model/Category.cs
+++ Abby/AbbyWeb/Model/Category.cs
@@ -8,6 +8,7 @@ namespace AbbyWeb.Model
         public int Id { get; set; }
         [Required]
         public string Name  { get; set; }
+        []
         public int DisplayOrder { get; set; }
     }
 }
